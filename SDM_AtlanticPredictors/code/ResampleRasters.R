3c27159dc3bf50429175f608131030a448b4dd98
Greyson <GreysonP@dfo-mpo.ca>
2020-01-10 19:17:40 +0000

    Finally got the resampling working

--- SDM_AtlanticPredictors/code/ResampleRasters.R
+++ SDM_AtlanticPredictors/code/ResampleRasters.R
@@ -12,21 +12,14 @@ library(moments) # required for skewness calculation
 wd <- "//ent.dfo-mpo.ca/ATLShares/Science/CESD/HES_MSP/R"
 setwd(wd)
 
-data.dir <- "./data/mar.wrangling"
-get_data('rv', data.dir = data.dir)
-# alternate site for the data:
-# data.dir <- "//dcnsbiona01a/BIODataSVC/IN/MSP/Projects/Aquaculture/SearchPEZ/inputs/mar.wrangling"
-
-
 # bring in OceanMask for clipping data and rasters
 dsn <- "./data/Boundaries"
 oceanMaskMAR <- readOGR(dsn,"ScotianShelfOceanMask_WithoutCoastalZone")
-oceanMaskATL <- readOGR() # NEED TO CONVERT GDB FC to shapefile
+# oceanMaskATL <- readOGR() # NEED TO CONVERT GDB FC to shapefile
 oceanMaskUTM <- spTransform(oceanMaskMAR,CRS("+init=epsg:26920"))
 
 prj <- CRS("+init=epsg:4326") # EPSG code for WGS84
 prjUTM <- CRS("+init=epsg:26920") # EPSG code for NAD83 UTM Zone 20
-
 
 bath <- 'R:/Science/CESD/HES_MSP/R/data/Projects/SDM_Pacific/predictors/bathy.asc'
 sst <- 'R:/Science/CESD/HES_MSP/R/data/Projects/SDM_Pacific/predictors/mean_jul_sst_2012_2018.tif'
@@ -34,31 +27,100 @@ chl <- 'R:/Science/CESD/HES_MSP/R/data/Projects/SDM_Pacific/predictors/mean_jul_
 sal <- 'U:/GIS/Projects/MSP/MSPData/NaturalResources/Climate/Sal_JulyMean_2012_14.tif' 
 tem <- 'U:/GIS/Projects/MSP/MSPData/NaturalResources/Climate/Temp_JulyMean_2012_14.tif' 
 
+rastNames <- c('bath','sst','chl','sal','tem')
 rastList <- list(bath,sst,chl,sal,tem)
-rastList <- c(bath,sst,chl,sal,tem)
-rastList <- c(bath)
+names(rastList) <- rastNames
+# turn the .tifs into RasterLayers
+rastList1 <- lapply(rastList,raster)
+# Set initial clipping extent from the smallest raster (bathy)
+names(rastList1)
+
+
+# See this post on alignning rasters:
+# https://gis.stackexchange.com/questions/158159/snapping-raster-grids-in-r
+
+# Load required libraries:
+#   
+# library(raster)
+# library(rgdal)
+# Read rasters:
+#   
+#   r1 = raster("./dir/r1.tif")
+# r2 = raster("./dir/r2.tif")
+# Resample to same grid:
+#   
+#   r.new = resample(r1, r2, "bilinear")
+# If required (for masking), set extents to match:
+#   
+#   ex = extent(r1)
+# r2 = crop(r2, ex)
+# Removed data which falls outside one of the rasters (if you need to):
+#   
+#   r.new = mask(r.new, r2)
+# Your rasters now match.
+
+ext <- extent(rastList1[[1]])
+Resamp <- rastList1[[1]]
+
+prjFun <- function(y) {
+  crs(y) <- prj
+  y <- resample(y,Resamp)
+  # y <- crop(y,ext)
+  y <- crop(y,oceanMaskMAR)
+  y <- mask(y,oceanMaskMAR)
+  if (names(y) == "Sal_JulyMean_2012_14") {
+    n <- names(y)
+    y <- calc(y, fun=function(x){ x[x < 25] <- NA; return(x)} )
+    names(y) <- n
+    } else if (names(y) == "Temp_JulyMean_2012_14") {
+      n <- names(y)
+      y <- calc(y, fun=function(x){ x[x < -2.5] <- NA; return(x)} )
+      names(y) <- n
+    }
+  
+  y <- projectRaster(y,crs = prjUTM,res = 1000)
+  #names(Ry) <- names(y)
+}
+
+rastList2 <- lapply(rastList1,prjFun)
+
+# Export these
+
+names(rastList2) <- rastNames
+rastList2
+
+dir <- "C:/Temp/"
+
+exportFun <- function(y) {
+  tif <- paste(dir,names(y),"4.tif",sep = "")
+  writeRaster(y,tif)
+  
+}
+
+lapply(rastList2,extent)
+lapply(rastList2,res)
+lapply(rastList2,summary)
+st <- stack(rastList2)  # works!!!
+
+lapply(rastList2,exportFun)
+names(rastList2[[2]])
+names(rastList2)
+
+
+# -------------------------------excess crud --------------------------------------------------
+y <- rastList[[2]]
 
 tem1 <- raster(tem)
 new <- prjFun(tem1)
-prjFun <- function(y) {
-  # Ry <- raster(y)
-  crs(y) <- prj
-  #crop(y,rastList1[[1]])
-  #Ry <- crop(y,oceanMaskMAR)
-  #Ry <- mask(Ry,oceanMaskMAR)
-  
-  #names(Ry) <- names(y)
-}
-plot(new)
 
-rastList2 <- lapply(rastList,prjFun)
 
 new1 <- new[new < -2.5] <- NA # this didn't work to make NA all the values below -2.5
 s <- calc(tem1, fun=function(x){ x[x < -2.5] <- NA; return(x)} ) # this doesn't go in function
 
 test <- -2.4
-rastList1 <- lapply(rastList,raster)
-rastList2 <- lapply(rastList1,prjFun) 
+rastList <- lapply(rastList,raster)
+
+rastList2 <- lapply(rastList,prjFun) 
 plot(rastList2[[1]])
 plot(rastList2[[2]])
 plot(rastList2[[3]])
@@ -70,6 +132,21 @@ plot(rastList2[[2]])
 plot(rastList2[[3]])
 plot(rastList2[[4]])
 plot(rastList2[[5]])
+
+plot(new)
+
+y <- rastList1[[4]]
+names(y)
+summary(y)
+y <- calc(y, fun=function(x){ x[x < 30] <- NA; return(x)} )
+
+
+names(rastList2)
+
+y <- rastList1[[3]]
+
+
+
 
 # The cropping and masking isn't working as it should
 CRS("+init=epsg:4326")
@@ -88,36 +165,19 @@ mask2_sstR <- mask(crop_sstR,oceanMaskMAR)
 plot(mask2_sstR)
 
 
-Get resolution of finest grid (chl - 0.0104)
-Project that to UTM, get resolution
-Then project all others to that resolution and projection (projectRaster)
+# Get resolution of finest grid (chl - 0.0104)
+# Project that to UTM, get resolution
+# Then project all others to that resolution and projection (projectRaster)
 
 
 # get raster resolution of all rasters
-lapply(rastList1,res)
+lapply(rastList2,res)
 # create a blank raster using projectExtent()
 # then projectRaster()
 extent1 <- extent(rastList1[[1]])
 lapply(rastList1,extent)
 rastList3 <- lapply(rastList1,crop,extent1)
 
-plot(rastList3[[1]])
-plot(rastList3[[2]])
-plot(rastList3[[3]])
-plot(rastList3[[4]])
-plot(rastList3[[5]])
+lapply(rastList1,names)
 
-plot(rastList1[[1]])
-
-  lapply(rastList1,names)
-
-
-
-# Crop all to the extent of bathy (it's the smallest raster)
-
-s <- crop(rastList1[[2]],extent(rastList1[[1]]))
-extent(s)
-extent(rastList1[[1]])
-
-rastList <- c(bath,sst,chl,sal,tem)
-rastList[[1]]
+# -------------------------------END excess crud ----------------------------------------------
