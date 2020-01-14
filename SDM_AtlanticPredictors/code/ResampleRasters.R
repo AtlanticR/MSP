ba2c018817391215eb159f2c0f9319bdb8095d4b
phyllog <phyllo@chebucto.ns.ca>
2020-01-14 19:10:45 +0000

    Cleaned up code and added in section for extracting out RV data for Barndoor skate (only 442 observations though)

--- SDM_AtlanticPredictors/code/ResampleRasters.R
+++ SDM_AtlanticPredictors/code/ResampleRasters.R
@@ -1,13 +1,8 @@
 library(rgdal) # to read shapefiles
 library(raster) # for the various raster functions
-library(dplyr) # all-purpose data manipulation (loaded after raster package since both have a select() function)
-library(gstat) # for IDW function
-library(sp) #Classes and methods for spatial data
-library(Mar.datawrangling)
-library(rgeos) # required for the dissolve argument in rasterToPolygon() according to help file
-library(moments) # required for skewness calculation
+library(sp) # Classes and methods for spatial data
 
-# Load RV data
+
 
 wd <- "//ent.dfo-mpo.ca/ATLShares/Science/CESD/HES_MSP/R"
 setwd(wd)
@@ -30,7 +25,7 @@ tem <- 'U:/GIS/Projects/MSP/MSPData/NaturalResources/Climate/Temp_JulyMean_2012_
 rastNames <- c('bath','sst','chl','sal','tem')
 rastList <- list(bath,sst,chl,sal,tem)
 names(rastList) <- rastNames
-# turn the .tifs into RasterLayers
+# turn the .tifs and .asc files into RasterLayers
 rastList1 <- lapply(rastList,raster)
 # Set initial clipping extent from the smallest raster (bathy)
 names(rastList1)
@@ -39,75 +34,124 @@ names(rastList1)
 # See this post on alignning rasters:
 # https://gis.stackexchange.com/questions/158159/snapping-raster-grids-in-r
 
-# Load required libraries:
-#   
-# library(raster)
-# library(rgdal)
-# Read rasters:
-#   
-#   r1 = raster("./dir/r1.tif")
-# r2 = raster("./dir/r2.tif")
-# Resample to same grid:
-#   
-#   r.new = resample(r1, r2, "bilinear")
-# If required (for masking), set extents to match:
-#   
-#   ex = extent(r1)
-# r2 = crop(r2, ex)
-# Removed data which falls outside one of the rasters (if you need to):
-#   
-#   r.new = mask(r.new, r2)
-# Your rasters now match.
+# get parameters of the bathymetry raster 
+# to use in the resample() function
 
-ext <- extent(rastList1[[1]])
-Resamp <- rastList1[[1]]
+Resamp <- rastList1[[1]] 
 
+
+# resample all rasters to the same extent (bathymetry layer)
+# crop and mask to the Scotian Shelf extent
+# project to UTM Zone 20, with a resolution of 1000m
 prjFun <- function(y) {
   crs(y) <- prj
   y <- resample(y,Resamp)
-  # y <- crop(y,ext)
   y <- crop(y,oceanMaskMAR)
   y <- mask(y,oceanMaskMAR)
-  if (names(y) == "Sal_JulyMean_2012_14") {
-    n <- names(y)
-    y <- calc(y, fun=function(x){ x[x < 25] <- NA; return(x)} )
-    names(y) <- n
-    } else if (names(y) == "Temp_JulyMean_2012_14") {
-      n <- names(y)
-      y <- calc(y, fun=function(x){ x[x < -2.5] <- NA; return(x)} )
-      names(y) <- n
-    }
-  
   y <- projectRaster(y,crs = prjUTM,res = 1000)
-  #names(Ry) <- names(y)
 }
 
+# apply the prjFun() function to each raster in the list
 rastList2 <- lapply(rastList1,prjFun)
 
-# Export these
-
+# name the rasters in the new list
 names(rastList2) <- rastNames
-rastList2
+
+rastList2[[1]] <- round(rastList2[[1]],0) # this isn't really needed since I can use the datatype argument
+# in writeRaster( , ,datatype = "INT2S")
+
+rastList2[[2]] <- round(rastList2[[2]],1)
+rastList2[[3]] <- round(rastList2[[3]],1)
+rastList2[[4]] <- round(calc(rastList2[[4]], fun=function(x){ x[x < 25] <- NA; return(x)} ),1)
+rastList2[[5]] <- round(calc(rastList2[[5]], fun=function(x){ x[x < -2.5] <- NA; return(x)} ),1)
+
+
+dir <- "C:/Temp/"
+
+# Write the rasters to tif files
+tif <- paste(dir,names(rastList2[1]),"_7.tif",sep = "")
+writeRaster(rastList2[[1]],tif,overwrite = TRUE,datatype = "INT2S")
+tif <- paste(dir,names(rastList2[2]),"_4.tif",sep = "")
+writeRaster(rastList2[[2]],tif,overwrite = TRUE)
+tif <- paste(dir,names(rastList2[3]),"_4.tif",sep = "")
+writeRaster(rastList2[[3]],tif,overwrite = TRUE)
+tif <- paste(dir,names(rastList2[4]),"_4.tif",sep = "")
+writeRaster(rastList2[[4]],tif,overwrite = TRUE)
+tif <- paste(dir,names(rastList2[5]),"_4.tif",sep = "")
+writeRaster(rastList2[[5]],tif,overwrite = TRUE)
+
+
+# Create a stack of the rasters and export as a single tif file (5 bands)
+st <- stack(rastList2)  # works!!!
+tif2="C:/Temp/Stack.tif"
+writeRaster(st,tif2)
+
+
+# select RV samples for barndoor skate and export as a shapefile
+library(rgdal) # to read shapefiles
+library(dplyr) # all-purpose data manipulation (loaded after raster package since both have a select() function)
+library(sp) #Classes and methods for spatial data
+library(Mar.datawrangling)
+
+
+# Load RV data
+data.dir <- "./data/mar.wrangling"
+get_data('rv', data.dir = data.dir)
+# alternate site for the data:
+# data.dir <- "//dcnsbiona01a/BIODataSVC/IN/MSP/Projects/Aquaculture/SearchPEZ/inputs/mar.wrangling"
+
+
+# Make copies of all the GS tables
+tmp_GSCAT <- GSCAT
+tmp_GSDET <- GSDET
+tmp_GSINF <- GSINF
+tmp_GSMISSIONS <- GSMISSIONS
+tmp_GSSPECIES <- GSSPECIES
+tmp_GSSTRATUM <- GSSTRATUM
+tmp_GSXTYPE <- GSXTYPE
+tmp_FGP_TOWS_NW2 <- FGP_TOWS_NW2
+
+
+# - Make oversize grid ----------------------
+# make grid of all SUMMER samples to get min and max extent
+clip_by_poly(db='rv', clip.poly = oceanMaskMAR) # clip data to the extent of the Ocean Mask
+GSSPECIES <- GSSPECIES[GSSPECIES$CODE==200,]
+GSXTYPE <- GSXTYPE[GSXTYPE$XTYPE==1,]
+self_filter(keep_nullsets = FALSE,quiet = TRUE)
+
+# Keep only the columns necessary
+# ("MISSION" "SETNO" "SDATE" "LATITUDE" "LONGITUDE")
+GSINF_all <- dplyr::select(GSINF,1:3,33:34)
+# Convert to the Spatial Object
+coordinates(GSINF_all) <- ~LONGITUDE+LATITUDE
+proj4string(GSINF_all) <- CRS("+init=epsg:4326") # Define coordinate system (WGS84)
+GSINF_allUTM <- spTransform(GSINF_all,CRS("+init=epsg:26920")) # Project to UTM
+
+
+
+
+
+
+
+
+
+
+# -------------------------------excess crud --------------------------------------------------
+
+
+# the export function didn't work as I'd wanted it to:
+
+# Export these
 
 dir <- "C:/Temp/"
 
 exportFun <- function(y) {
-  tif <- paste(dir,names(y),"4.tif",sep = "")
-  writeRaster(y,tif)
+  tif <- paste(dir,names(y),"_4.tif",sep = "")
+  writeRaster(y,tif,overwrite = TRUE)
   
 }
 
-lapply(rastList2,extent)
-lapply(rastList2,res)
-lapply(rastList2,summary)
-st <- stack(rastList2)  # works!!!
 
-lapply(rastList2,exportFun)
-names(rastList2[[2]])
-names(rastList2)
-
-
-# -------------------------------excess crud --------------------------------------------------
 y <- rastList[[2]]
 
 tem1 <- raster(tem)
