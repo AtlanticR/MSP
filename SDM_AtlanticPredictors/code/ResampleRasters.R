5196600b33bac458edcb79196bdc6dd58ca9da8d
phyllog <phyllo@chebucto.ns.ca>
2020-01-23 18:48:11 +0000

    Updated code

--- SDM_AtlanticPredictors/code/ResampleRasters.R
+++ SDM_AtlanticPredictors/code/ResampleRasters.R
@@ -3,13 +3,14 @@ library(raster) # for the various raster functions
 library(sp) # Classes and methods for spatial data
 
 
-
+# This is my R drive
 wd <- "//ent.dfo-mpo.ca/ATLShares/Science/CESD/HES_MSP/R"
 setwd(wd)
 
 # bring in OceanMask for clipping data and rasters
 dsn <- "./data/Boundaries"
-oceanMaskMAR <- readOGR(dsn,"ScotianShelfOceanMask_WithoutCoastalZone")
+# oceanMaskMAR <- readOGR(dsn,"ScotianShelfOceanMask_WithoutCoastalZone")
+oceanMaskMAR <- readOGR(dsn,"ScotianShelfOceanMask_WithoutCoastalZone_Edit")
 # oceanMaskATL <- readOGR() # NEED TO CONVERT GDB FC to shapefile
 oceanMaskUTM <- spTransform(oceanMaskMAR,CRS("+init=epsg:26920"))
 
@@ -22,6 +23,8 @@ sst <- 'R:/Science/CESD/HES_MSP/R/data/Projects/SDM_Pacific/predictors/mean_jul_
 chl <- 'R:/Science/CESD/HES_MSP/R/data/Projects/SDM_Pacific/predictors/mean_jul_chl_2012_2018.tif'
 sal <- 'U:/GIS/Projects/MSP/MSPData/NaturalResources/Climate/Sal_JulyMean_2012_14.tif' 
 tem <- 'U:/GIS/Projects/MSP/MSPData/NaturalResources/Climate/Temp_JulyMean_2012_14.tif' 
+temOld <- 'U:/GIS/Projects/MSP/MSPData/NaturalResources/Climate/Temp_JulyMean_2012_14_Old.tif'
+
 
 rastNames <- c('bath','sst','chl','sal','tem')
 rastList <- list(bath,sst,chl,sal,tem)
@@ -63,47 +66,32 @@ rastList2[[1]] <- round(rastList2[[1]],0) # this isn't really needed since I can
 
 rastList2[[2]] <- round(rastList2[[2]],1)
 rastList2[[3]] <- round(rastList2[[3]],1)
-rastList2[[4]] <- round(calc(rastList2[[4]], fun=function(x){ x[x < 25] <- NA; return(x)} ),1)
-rastList2[[5]] <- round(calc(rastList2[[5]], fun=function(x){ x[x < -2.5] <- NA; return(x)} ),1)
-
-resampFun <- function(y) {
-  y <- y >-9999
-  #table(as.vector(y))
-  poly <- rasterToPolygons(y,na.rm = TRUE, dissolve = TRUE)
-  # writeOGR(poly,dsn,paste("Poly_",names(y),sep = ""),driver="ESRI Shapefile", overwrite_layer = TRUE)
-}
+rastList2[[4]] <- round(rastList2[[4]],1)
+rastList2[[5]] <- round(rastList2[[5]],1)
 
 
-polyList3 <- lapply(rastList2,resampFun)
-names(polyList3) <- rastNames
+plot(rastList2[[1]])
+plot(rastList2[[2]])
+plot(rastList2[[3]])
+plot(rastList2[[4]])
+plot(rastList2[[5]])
 
-writeOGR(poly,dsn,paste("Poly_",names(polyList3[i]),sep = ""),driver="ESRI Shapefile", overwrite_layer = TRUE)
-
-i = 2
-names(rastList2) <- rastNames
+dsn <-  "//ent.dfo-mpo.ca/ATLShares/Science/CESD/HES_MSP/R/SDM_PacificMARCollaboration/DataInput"
 
 start_time <- Sys.time()
 for(i in 1:5){
-  #rast <- rastList2[[i]] > -9999
-  poly <- polyList3[[i]]
+  rast <- rastList2[[i]] > -9999 # WHAT'S THIS FOR?
+  poly <- rasterToPolygons(rast,na.rm = TRUE, dissolve = TRUE)
   writeOGR(poly,dsn,paste("Poly_",names(rastList2[i]),sep = ""),driver="ESRI Shapefile", overwrite_layer = TRUE)
 }
 end_time <- Sys.time()
 end_time - start_time
 
-writeOGR(poly,"//ent.dfo-mpo.ca/ATLShares/Science/CESD/HES_MSP/R/SDM_PacificMARCollaboration/DataInput","Poly1",driver="ESRI Shapefile", overwrite_layer = TRUE)
 
-plot(poly)
+
 SumFun <- function(y) {
   table(as.vector(y))
 }
-
-polyList3 <- lapply(rastList2,resampFun)
-
-bathPoly <- rasterToPolygons(rastList3[[1]], na.rm = TRUE, dissolve = TRUE)
-
-#dir <- "C:/Temp/"
-dsn <-  "//ent.dfo-mpo.ca/ATLShares/Science/CESD/HES_MSP/R/SDM_PacificMARCollaboration/DataInput"
 
 # Write the rasters to tif files
 tif <- paste(dsn,"/",names(rastList2[1]),"_1.tif",sep = "")
@@ -123,48 +111,24 @@ st <- stack(rastList2)  # works!!!
 tif2="C:/Temp/Stack.tif"
 writeRaster(st,tif2)
 
-rastList2[[1]]
-as.data.frame(table(as.vector(rastList2[[1]])))
-
 
 # select RV samples for barndoor skate and export as a shapefile
 
 library(dplyr) # all-purpose data manipulation (loaded after raster package since both have a select() function)
-library(sp) #Classes and methods for spatial data
 library(Mar.datawrangling)
 
 
 # Load RV data
 data.dir <- "./data/mar.wrangling"
-get_data('rv', data.dir = data.dir)
+get_data('rv', data.dir = data.dir, quiet = TRUE)
 # alternate site for the data:
 # data.dir <- "//dcnsbiona01a/BIODataSVC/IN/MSP/Projects/Aquaculture/SearchPEZ/inputs/mar.wrangling"
 
 
-# Make copies of all the GS tables
-tmp_GSCAT <- GSCAT
-tmp_GSDET <- GSDET
-tmp_GSINF <- GSINF
-tmp_GSMISSIONS <- GSMISSIONS
-tmp_GSSPECIES <- GSSPECIES
-tmp_GSSTRATUM <- GSSTRATUM
-tmp_GSXTYPE <- GSXTYPE
-tmp_FGP_TOWS_NW2 <- FGP_TOWS_NW2
-
-# Restore original GS tables for filtering
-GSCAT <- tmp_GSCAT
-GSDET <- tmp_GSDET
-GSINF <- tmp_GSINF
-GSMISSIONS <- tmp_GSMISSIONS
-GSSPECIES <- tmp_GSSPECIES
-GSSTRATUM <- tmp_GSSTRATUM
-GSXTYPE <- tmp_GSXTYPE
-FGP_TOWS_NW2 <- tmp_FGP_TOWS_NW2
-
 yearb <- 2012
 yeare <- 2019
 clip_by_poly(db='rv', clip.poly = oceanMaskMAR) # clip data to the extent of the Ocean Mask
-GSMISSIONS <- GSMISSIONS[GSMISSIONS$YEAR >= yearb & GSMISSIONS$YEAR < yeare,]
+GSMISSIONS <- GSMISSIONS[GSMISSIONS$YEAR >= yearb & GSMISSIONS$YEAR < yeare & GSMISSIONS$SEASON=="SUMMER",]
 GSXTYPE <- GSXTYPE[GSXTYPE$XTYPE==1,]
 self_filter(keep_nullsets = FALSE,quiet = TRUE) # 659 in GSCAT
 GSINF2 <- GSINF # make copy of GSINF to use for a full set of samples
@@ -220,124 +184,8 @@ allCatchUTM<-spTransform(allCatch,CRS("+init=epsg:26920"))
 # Export the shapefile - NOTE, this was done to compare with ArcGIS processing
 # print(paste("Loop ",count, " - exporting shapefile",sep = ""))
 
-writeOGR(allCatchUTM,"//ent.dfo-mpo.ca/ATLShares/Science/CESD/HES_MSP/R/SDM_PacificMARCollaboration/DataInput","Species200",driver="ESRI Shapefile")
 writeOGR(allCatchUTM,dsn,"Species200",driver="ESRI Shapefile")
 
 
+# - ------------------------------END Get Barndoor skate RV data ----------------------
 
-
-
-# - ---------------------------------- Get Barndoor skate RV data ----------------------
-# make grid of all SUMMER samples to get min and max extent
-clip_by_poly(db='rv', clip.poly = oceanMaskMAR) # clip data to the extent of the Ocean Mask
-GSSPECIES <- GSSPECIES[GSSPECIES$CODE==200,] # barndoor skate
-# GSSPECIES <- GSSPECIES[GSSPECIES$CODE==10,] # cod
-GSXTYPE <- GSXTYPE[GSXTYPE$XTYPE==1,]
-self_filter(keep_nullsets = FALSE,quiet = TRUE)
-
-# Keep only the columns necessary
-# ("MISSION" "SETNO" "SDATE" "LATITUDE" "LONGITUDE")
-GSINF_all <- dplyr::select(GSINF,1:3,33:34)
-# Convert to the Spatial Object
-coordinates(GSINF_all) <- ~LONGITUDE+LATITUDE
-proj4string(GSINF_all) <- CRS("+init=epsg:4326") # Define coordinate system (WGS84)
-GSINF_allUTM <- spTransform(GSINF_all,CRS("+init=epsg:26920")) # Project to UTM
-
-
-
-
-
-
-
-
-
-
-# -------------------------------excess crud --------------------------------------------------
-
-
-# the export function didn't work as I'd wanted it to:
-
-# Export these
-
-dir <- "C:/Temp/"
-
-exportFun <- function(y) {
-  tif <- paste(dir,names(y),"_1.tif",sep = "")
-  writeRaster(y,tif,overwrite = TRUE)
-  
-}
-
-
-y <- rastList[[2]]
-
-tem1 <- raster(tem)
-new <- prjFun(tem1)
-
-
-new1 <- new[new < -2.5] <- NA # this didn't work to make NA all the values below -2.5
-s <- calc(tem1, fun=function(x){ x[x < -2.5] <- NA; return(x)} ) # this doesn't go in function
-
-test <- -2.4
-rastList <- lapply(rastList,raster)
-
-rastList2 <- lapply(rastList,prjFun) 
-plot(rastList2[[1]])
-plot(rastList2[[2]])
-plot(rastList2[[3]])
-plot(rastList2[[4]])
-plot(rastList2[[5]])
-
-plot(rastList2[[1]])
-plot(rastList2[[2]])
-plot(rastList2[[3]])
-plot(rastList2[[4]])
-plot(rastList2[[5]])
-
-plot(new)
-
-y <- rastList1[[4]]
-names(y)
-summary(y)
-y <- calc(y, fun=function(x){ x[x < 30] <- NA; return(x)} )
-
-
-names(rastList2)
-
-y <- rastList1[[3]]
-
-
-
-
-# The cropping and masking isn't working as it should
-CRS("+init=epsg:4326")
-rastList2 <- lapply(rastList1,crop, oceanMaskATL)
-
-sstR <- raster(sst)
-
-crop_sstR <- crop(sstR,oceanMaskMAR)
-mask_sstR <- mask(sstR,oceanMaskMAR)
-plot(crop_sstR)
-plot(mask_sstR)
-# perhaps I want to Crop first (get it down to a small extent)
-# and then Mask (create NAs outside of the polygon)
-
-mask2_sstR <- mask(crop_sstR,oceanMaskMAR)
-plot(mask2_sstR)
-
-
-# Get resolution of finest grid (chl - 0.0104)
-# Project that to UTM, get resolution
-# Then project all others to that resolution and projection (projectRaster)
-
-
-# get raster resolution of all rasters
-lapply(rastList2,res)
-# create a blank raster using projectExtent()
-# then projectRaster()
-extent1 <- extent(rastList1[[1]])
-lapply(rastList1,extent)
-rastList3 <- lapply(rastList1,crop,extent1)
-
-lapply(rastList1,names)
-
-# -------------------------------END excess crud ----------------------------------------------
