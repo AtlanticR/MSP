ee51ea260b18c5aa6aa20e41bc9029996f2a6342
phyllog <phyllog@users.noreply.github.com>
2020-06-02 12:54:25 -0300

    Removed odd bits of code added by git

--- SpatialDataSynopsis/code/src/fn_RestoreTables.r
+++ SpatialDataSynopsis/code/src/fn_RestoreTables.r
@@ -15,21 +15,4 @@ RestoreTables <- function(){
   }
   )
   return(NULL)
-<<<<<<< HEAD
 }
-
-
-# old attempt
-# 
-# RestoreDataTables <- function() {
-#   GSCAT <- tmp_GSCAT
-#   GSDET <- tmp_GSDET
-#   GSINF <- tmp_GSINF
-#   GSMISSIONS <- tmp_GSMISSIONS
-#   GSSPECIES <- tmp_GSSPECIES
-#   GSSTRATUM <- tmp_GSSTRATUM
-#   GSXTYPE <- tmp_GSXTYPE
-# }
-=======
-}
->>>>>>> 80d769c9bf90ed7c23263d4a4379838e60ac4c79
