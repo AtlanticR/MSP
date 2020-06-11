75ee4e546536a0e0285bffa590fcd86cb20faa96
gomezcatalina <gomezcatalina@gmail.com>
2020-06-11 20:09:54 +0100

    Search PEZ updated to explore its use for emergency response: Port Hawkesbury Oil Spill Exercise.

--- SearchPEZ/code/get_data.R
+++ SearchPEZ/code/get_data.R
@@ -1,20 +1,20 @@
 #This chunk of code should be run to do a re-extraction of data or to extract data using a new polygon.
 #Note taht you need 64-bit ODBC driver to be able to extract MARFIS data. Mike provided data to Catalina on Sept 2019 and waiting to get Oracle 64 bit to do extraction myself.
-
-rm(list=ls(all=TRUE))
 library(Mar.datawrangling)
 library(rgdal)
-AquaSiteName <- "Whycocomagh"
-PEZversion <- "5km"
-cat(AquaSiteName)
-polyPath <- "//ent.dfo-mpo.ca/ATLShares/Science/CESD/HES_MSP/R/SearchPEZ/inputs/polygons"
 
+#polyPath <- "//ent.dfo-mpo.ca/ATLShares/Science/CESD/HES_MSP/R/SearchPEZ/inputs/polygons"
+polyPath <- "C:/RProjects/data/SearchPEZpolygons"
+AquaSiteName <- "FarmersLedge"
+PEZversion <- "4748m"
+cat(AquaSiteName)
 pl <- list.files(polyPath,"*.shp")
 pl <- pl[-grep("xml",pl)]
 PEZ_poly <- readOGR(file.path(polyPath,pl[grep(paste0("PEZ_",AquaSiteName,PEZversion),pl)]))
 site <- readOGR(polyPath,layer=paste0("Site_",AquaSiteName))
 
-source("C:/RProjects/MSP/SearchPEZ/code/.Rprofile") #Catalina's path to access Oracle passwords
+source("C:/Users/GomezC/Documents/.Rprofile") #Catalina's path to access Oracle passwords
+data.dir = "C:/RProjects/data/mar.wrangling"
 get_data(db = 'rv', data.dir = data.dir)
 GSINF = clip_by_poly(df = GSINF
                      , lat.field = "LATITUDE"
