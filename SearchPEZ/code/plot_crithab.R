a709befc1cd990e2378d0107a402beefd436c0d7
Gregory N. Puncher <74623208+gpuncher@users.noreply.github.com>
2020-12-21 15:00:00 -0400

    Update plot_crithab.R

--- SearchPEZ/code/plot_crithab.R
+++ SearchPEZ/code/plot_crithab.R
@@ -7,7 +7,7 @@ geom_sf(data=ClippedCritHab,fill="red",col="red")+
 geom_sf(data=PEZ_poly_st,fill=NA, col="blue", size=1)+
 geom_sf(data=ClippedCritLand,fill=c("grey90"), col="black")+
 theme_bw()+
-coord_sf(xlim = c(-71, -56.5), ylim = c(42, 51))+
+coord_sf(xlim = c(-67.6, -56.5), ylim = c(42, 47.7))+
 labs(x=expression(paste("Longitude ",degree,"W",sep="")),
      y=expression(paste("Latitude ",degree,"N",sep="")),
      col="")
