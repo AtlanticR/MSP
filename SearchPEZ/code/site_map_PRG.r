91a145028991fe62ffaacef39abac4688a01cab1
Greyson <Philip.Greyson@dfo-mpo.gc.ca>
2021-03-10 13:47:48 -0400

    Created new setup.r for my work updated site_map_PRG.r and troubleshoot.rmd and plot_wsdb_PRG.R

--- SearchPEZ/code/site_map_PRG.r
+++ SearchPEZ/code/site_map_PRG.r
@@ -14,38 +14,35 @@
 # Output: map 
 
 
-site_map <- function(studyArea,studyArea_st,site,land,buf) {
+site_map <- function(studyArea,site,land,buf) {
 
   # buf is in km, and now converted to degrees
-  buf=buf/100
-  #png("pez_and_site.png", width=1616, height=1410)
- 
-  
-  # PRG use the sf object to get bounding box 
-  # bounding box
-  bb=as.data.frame(summary(studyArea)$bbox)
-  bb=as.data.frame(st_bbox(studyArea))
-  bb=st_as_sfc(st_bbox(studyArea))
+  buf <- buf/100
+
+  # get bounding box
   bb <- st_bbox(studyArea)
   
-  bb[[1]]
-  
   # buffer around bounding box
-  #buf=0.05
+  # buf <- buf/100
   
   # longitude and latitude limits for the map
-  lonLim=c(bb$min[1]-buf, bb$max[1]+buf)
-  latLim=c(bb$min[2]-buf, bb$max[2]+buf)
-  longmin<-bb$min[1]-buf
-  longmax<-bb$max[1]+buf
-  latmin<-bb$min[2]-buf
-  latmax<-bb$max[2]+buf
+  lonLim <- c(bb[[1]]-buf, bb[[3]]+buf)
+  latLim <- c(bb[[2]]-buf, bb[[4]]+buf)
+
+  
+  # longitude and latitude limits for the map
+  lonLim <- c(bb[[1]]-buf, bb[[3]]+buf)
+  latLim <- c(bb[[2]]-buf, bb[[4]]+buf)
+  longmin<-bb[[1]]-buf
+  longmax<-bb[[3]]+buf
+  latmin<-bb[[2]]-buf
+  latmax<-bb[[4]]+buf
   
 ggplot()+
-  geom_sf(data=studyArea_st,fill="deepskyblue", col="black", size=0.6, alpha=0.4)+
+  geom_sf(data=studyArea,fill="deepskyblue", col="black", size=0.6, alpha=0.4)+
   geom_sf(data=site,fill="yellow",col="black", size=0.6)+
   geom_sf(data=land,fill=c("lightgrey"), col="black", size=0.7)+
-  watermark(show = TRUE, lab = "DFO Internal Use Only")+
+  #watermark(show = TRUE, lab = "DFO Internal Use Only")+
   annotation_scale(location="bl")+
   theme_bw()+
   coord_sf(xlim = c(longmin, longmax), ylim = c(latmin, latmax))+
